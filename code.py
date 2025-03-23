d83622b1fbb0b7c801f031d733d88a5dffe669ab
R0Master <128303758+R0Master@users.noreply.github.com>
2025-03-23 23:36:14 +0300

    code.py

---
+++ code.py
@@ -0,0 +1,25 @@
+from math import *
+
+number = int(input())
+action = input()
+number2 = int(input())
+
+
+def root(x, y):
+    return x ** (1 / y)
+
+
+if action == '+':
+    print('Result:', number + number2)
+elif action == '-':
+    print('Result:', number - number2)
+elif action == '*':
+    print('Result:', number * number2)
+elif action == '/':
+    print('Result:', number / number2)
+elif action == '/' and number2 == 0:
+    print('ERROR! DIVISION BY ZERO!')
+elif action == '^':
+    print('Result:', number**number2)
+elif action == 'root':
+    print('Result:', ceil(root(number, number2)) if root(number, number2) * 100 % 100 > 98 else root(number, number2))
